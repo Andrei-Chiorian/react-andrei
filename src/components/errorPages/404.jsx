633cb3ba08baa3e87b341c9dd2f4743cd6a450b0
Andrei Chiorian <achiorian23@yahoo.com>
2024-01-10 22:34:00 +0100

    nav-bar-sticky

--- src/components/errorPages/404.jsx
+++ src/components/errorPages/404.jsx
@@ -2,8 +2,12 @@ import './404.css';
 
 export function Error404() {
   return (
-    <div>
-        404, No hay naaaaa
+    
+    <div className='error-404-container'>
+      <div className='error-404-wrapper'>
+          <img className='error-400' src="./14463951.jpg" alt="" />
+      </div>
+        
     </div>
   );
 }
